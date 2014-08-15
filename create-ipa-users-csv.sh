2893e55024320d9ff7827e53cf965af2cb700cb0
Thorsten Scherf <tscherf@redhat.com>
2014-08-15 17:32:16 +0200

    added some sanity checking

--- create-ipa-users-csv.sh
+++ create-ipa-users-csv.sh
@@ -8,6 +8,18 @@ pwlist=ipa-create-users-passwords.txt # file with initial random passwords
 
 ### END ###
 
+# check if tgt and csv files are available
+if ( ! klist -s ); then
+	echo -e "No TGT available. Run kinit and try again.\n"
+	exit 1
+fi
+
+if [ ! -f $csvfile ]; then
+	echo -e "No CSV file available. Exiting.\n"
+	exit 1
+fi
+
+# Create the users   
 while IFS=, read first last login email
 do
     echo "* Creating user $login"
@@ -19,7 +31,8 @@ do
     echo "--" >> $pwlist
 done < $csvfile
 
-echo -e "Passwords are stored in file $pwlist. \n"
-
+if [ -f $pwlist ]; then
+    echo -e "Passwords are stored in file $pwlist. \n"
+fi
 
 
