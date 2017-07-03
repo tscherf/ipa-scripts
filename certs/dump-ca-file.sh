59c93ae0b11d07ed66483366454f8461aebce86e
Thorsten Scherf <tscherf@redhat.com>
2017-07-03 09:25:45 +0200

    fixed trust store search script

--- certs/dump-ca-file.sh
+++ certs/dump-ca-file.sh
@@ -12,7 +12,7 @@ cat $1 | while read line; do
     cert=$cert$line$'\n'
 
     if [ "$line" != "${line/END CERTIFICATE/}" ]; then
-        echo "$cert" | openssl x509 -subject -issuer -dates -purpose -noout
+        echo "$cert" | openssl x509 -subject -issuer -dates -purpose -serial -noout
         echo "-------"
         cert=""
     fi
--- certs/search-cert-in-trust-store.sh
+++ certs/search-cert-in-trust-store.sh
@@ -1,1 +1,2 @@
-openssl crl2pkcs7 -nocrl -certfile /etc/pki/tls/certs/ca-bundle.crt | openssl pkcs7 -print_certs -noout | grep 'subject=/O=GSSLAB.FAB.REDHAT.COM/CN=Certificate Authority'
+openssl crl2pkcs7 -nocrl -certfile /etc/pki/tls/certs/ca-bundle.crt | \
+    openssl pkcs7 -noout -print_certs
